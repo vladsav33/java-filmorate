ac36965044ec3177a39127a3f6e985be4ba3a286
pavelgreblov <greblovpn@yandex.ru>
2023-04-14 18:07:34 +0300

    refactor: move to impl package and deprecate inmemory implementation

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/InMemoryFilmStorage.java
@@ -1,8 +1,10 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.dao.impl;
 
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.FilmStorage;
 import ru.yandex.practicum.filmorate.model.Film;
 
-@Component
+@Deprecated
+@Component("inMemoryFilmStorage")
 public class InMemoryFilmStorage extends BaseModelStorage<Film> implements FilmStorage {
 }
--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/InMemoryUserStorage.java
@@ -1,8 +1,10 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.dao.impl;
 
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dao.UserStorage;
 import ru.yandex.practicum.filmorate.model.User;
 
-@Component
+@Deprecated
+@Component("inMemoryUserStorage")
 public class InMemoryUserStorage extends BaseModelStorage<User> implements UserStorage {
 }
