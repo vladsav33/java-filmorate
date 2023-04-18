6446468781ec7da9b31ede950a6bd803c3126a1b
pavelgreblov <greblovpn@yandex.ru>
2023-04-18 23:33:46 +0300

    refactor: add @NoArgsConstructor

--- src/main/java/ru/yandex/practicum/filmorate/model/BaseModel.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/BaseModel.java
@@ -2,12 +2,11 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public abstract class BaseModel {
     private int id;
-
-    BaseModel() {
-    }
 }
